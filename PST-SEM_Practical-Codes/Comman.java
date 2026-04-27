f110ea810eea3fa91044caffa5e3cc7e778b3002
vtu24372 <vtu24372@veltech.edu.in>
2026-04-27 12:07:13 +0530

    PST_Java-Codes

--- PST-SEM_Practical-Codes/Comman.java
+++ PST-SEM_Practical-Codes/Comman.java
@@ -3,31 +3,10 @@ import java.util.*;
 class Comman{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
-        String s = sc.nextLine();
-        int max = Integer.MIN_VALUE, sum = 0;
-        int start = 0, l = 0, r = 0;
 
-        for(int i=0; i<s.length(); i++){
-            int val = Character.isLowerCase(s.charAt(i)) 
-            ? s.charAt(i) - 'a' +1 
-            : - (s.charAt(i) - 'A' +1);
+       String text = sc.nextLine();
+       String pat = sc.nextLine();
 
-            if(sum < sum+val){
-                sum = val;
-                start = i;
-            } else{
-                sum += val;
-            }
-
-            if(sum > max){
-                max = sum;
-                l = start;
-                r = i;
-            }
-        }
-        System.out.println(max);
-        System.out.println(s.substring(l,r+1));
-        
+       System.out.print(text.indexOf(pat));
     }
 }
